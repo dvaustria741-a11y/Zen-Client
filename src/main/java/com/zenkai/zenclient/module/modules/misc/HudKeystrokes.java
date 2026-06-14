5721d8a8614dfa3aea8a57c8bbe862f4c9755bf3
dvaustria741-a11y <dvaustria741@gmail.com>
2026-06-14 20:39:14 +0800

    feat: HudKeystrokes

---
+++ src/main/java/com/zenkai/zenclient/module/modules/misc/HudKeystrokes.java
@@ -0,0 +1,29 @@
+package com.zenkai.zenclient.module.modules.misc;
+
+import com.zenkai.zenclient.ZenClient;
+import com.zenkai.zenclient.hud.HudElement;
+import com.zenkai.zenclient.module.Category;
+import com.zenkai.zenclient.module.Module;
+import org.lwjgl.input.Keyboard;
+
+/** Misc module that shows/hides the Keystrokes HUD element. */
+public final class HudKeystrokes extends Module {
+
+    public HudKeystrokes() {
+        super("HUD Keystrokes", "Toggle the Keystrokes HUD.", Category.MISC, Keyboard.KEY_NONE);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        HudElement el = ZenClient.getInstance().getHudManager().getElement("Keystrokes");
+        if (el != null) el.setVisible(true);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        HudElement el = ZenClient.getInstance().getHudManager().getElement("Keystrokes");
+        if (el != null) el.setVisible(false);
+    }
+}
